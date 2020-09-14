d374b42823c8e176a6d13280d1b25d1e3d089e6e
jefflub <jeff@goyesler.com>
2020-09-14 23:12:26 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+# Convert me to project mode before running!
+# https://repl.it/site/docs/files#project-mode
+
+require 'sinatra'
+
+set :protection, :except => :frame_options
+set :bind, '0.0.0.0'
+set :port, 8080
+
+get '/' do
+  'Hello world!'
+end
+
+get '/test' do
+  'Goodbye cruel world!'
+end
